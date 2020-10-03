2339c42ac3ff65ccad2532f3dd4094e56f78a95a
Catherine France <catherinefoster95@gmail.com>
2020-10-03 14:03:47 +0100

    challenge completed and some animation

--- Week 1 - Emoji Ratings/index.js
+++ Week 1 - Emoji Ratings/index.js
@@ -19,17 +19,62 @@ focus, focusout, DOM manipulation, tabindex
 const box = document.getElementById("box")
 const text = document.getElementById("text")
 
-box.addEventListener("focus", function(){
+box.addEventListener("focus", function () {
     text.textContent = "Type a number between 1 and 5"
 })
 
-box.addEventListener("focusout", function(){
+box.addEventListener("focusout", function () {
     text.textContent = "Click here to give your rating"
 })
 
 
 // Write your code here 👇
+box.addEventListener("keyup", function (e) {
+    if (e.keyCode > 48 && e.keyCode < 54) {
+        var rating = e.keyCode - 48
+        ShowEmoji(rating)
+    }
+    else {
+        HideEmoji()
+    }
+})
 
+function ShowEmoji(rating) {
+    var emoji
+    switch (rating) {
+        case 1: {
+            emoji = "🤬"
+            break
+        }
+        case 2: {
+            emoji = "☹️"
+            break
+        }
+        case 3: {
+            emoji = "😐"
+            break
+        }
+        case 4: {
+            emoji = "🙂"
+            break
+        }
+        case 5: {
+            emoji = "😁"
+            break
+        }
+        default: {
+            emoji = "Oops, something went wrong!"
+        }
+    }
+
+    text.textContent = emoji
+    text.classList.add("spin")
+    setTimeout(() => { text.classList.remove("spin"); }, 1000);
+}
+
+function HideEmoji() {
+    text.textContent = "Type a number between 1 and 5"
+}
 
 /*
 
@@ -39,7 +84,7 @@ DETAILED INSTRUCTIONS
 3. If it's between 1 and 5, display an emoji in the box!
 
 STRETCH GOALS:
-- Animate the emoji onto the screen! Why not go crazy with multiple emojis? 
+- Animate the emoji onto the screen! Why not go crazy with multiple emojis?
 - Reset the entire app when box doesn't have focus anymore
 - Can you improve the overall design?
 
